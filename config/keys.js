3f3bdcc0fe0da18156e8eeec4b17b2644e40279e
Akash Sawant <sawantakash321@gmail.com>
2018-09-06 20:21:44 +0530

    Added regiter user backend code

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb://akash:dontbelazy321@ds145562.mlab.com:45562/devconnect"
+	mongoURI: 'mongodb://akash:dontbelazy321@ds145562.mlab.com:45562/devconnect'
 };
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const profile = require("./routes/api/profile");
-const users = require("./routes/api/users");
-const posts = require("./routes/api/posts");
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send('Hello World'));
 
 // Use Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
