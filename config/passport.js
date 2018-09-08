4574b6d0d8b811ed6851f32e0bb17968658c405f
Akash Sawant <sawantakash321@gmail.com>
2018-09-08 18:30:06 +0530

    Validated login and register routed and added profile mongoose model

--- config/passport.js
+++ config/passport.js
@@ -11,15 +11,14 @@ opts.secretOrKey = key.secretOrkey;
 module.exports = passport => {
 	passport.use(
 		new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
-        .then(user => {
-  				if (user) {
-	  				return done(null, user);
-		  		}
-			  	return done(null, false);
-      })
-      .catch(err => console.log(err);
-      )
+			User.findById(jwt_payload.id)
+				.then(user => {
+					if (user) {
+						return done(null, user);
+					}
+					return done(null, false);
+				})
+				.catch(err => console.log(err));
 		})
 	);
 };
