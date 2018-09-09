9a77bfc28fa05a43d0d9cc0f6196fc4dc66903bf
Akash Sawant <sawantakash321@gmail.com>
2018-09-09 14:51:03 +0530

    Added likes and comment backend functionality

--- models/Post.js
+++ models/Post.js
@@ -16,41 +16,41 @@ const PostSchema = new Schema({
 	},
 	avatar: {
 		type: String
-  }
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-		    ref: 'users'
-      }
-    }
-  ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-		    ref: 'users'
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
-    }
-  ],
-  date: {
-    type: Date,
-    default: Date.now
-  }
+	},
+	likes: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'users'
+			}
+		}
+	],
+	comments: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'users'
+			},
+			text: {
+				type: String,
+				required: true
+			},
+			name: {
+				type: String
+			},
+			avatar: {
+				type: String
+			},
+			date: {
+				type: Date,
+				default: Date.now
+			}
+		}
+	],
+	date: {
+		type: Date,
+		default: Date.now
+	}
 });
 
-module.exports = Post = mongoose.model('post', PostSchema)
+module.exports = Post = mongoose.model('post', PostSchema);
